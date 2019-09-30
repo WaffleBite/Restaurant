0e8a1abff19eb929e053d66ab95f7a9e72ddaaab
Karolina <karroliina.k@gmail.com>
2019-09-30 08:56:54 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace Restaurant
     {
         static void Main(string[] args)
         {
-            
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+
+                Console.WriteLine("[1] Add order");
+                Console.WriteLine("[2] List order");
+                Console.WriteLine("[3] Exit");
+
+                ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+                Console.Clear();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+
+                        break;
+                }
+            }
         }
     }
 }
